b0c7907374a3aa0d4ad8152da27b883655aae4b3
Felipe Negrelle <felipenegrelleramos@gmail.com>
2026-08-24 00:48:57 -0300

    feat(persistence): configure JPA and database schema resources

---
+++ src/main/java/com/felipe/uniroom/repositories/DatabaseRepository.java
@@ -0,0 +1,213 @@
+package com.felipe.uniroom.repositories;
+
+import com.felipe.uniroom.config.ConnectionManager;
+import com.felipe.uniroom.config.Role;
+import com.felipe.uniroom.entities.Branch;
+import com.felipe.uniroom.entities.Corporate;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
+import jakarta.transaction.Transactional;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+//TODO talvez tornar esta classe do tipo <T> e fazer como no spring boot
+@Transactional
+public class DatabaseRepository {
+    public static <T> List<T> findAll(Class<T> entity, Role role) {
+        try (EntityManager em = ConnectionManager.getEntityManager()) {
+            final Map<String, Object> params = new HashMap<>();
+            final StringBuilder queryString = new StringBuilder("SELECT e FROM ").append(entity.getSimpleName()).append(" e WHERE TRUE");
+
+            switch (role.getRole()) {
+                case 'A':
+                    // Administrador tem acesso a todos os registros, então não precisa de filtro
+                    break;
+                case 'C':
+                    if (!entity.getSimpleName().equals("Corporate")) {
+                        boolean hasBranch = false;
+
+                        for (Field f : entity.getDeclaredFields()) {
+                            if (f.getName().equals("branch")) {
+                                hasBranch = true;
+                                break;
+                            }
+                        }
+
+                        if (hasBranch) {
+                            queryString.append(" AND e.branch.corporate IN :corporates");
+                            params.put("corporates", role.getCorporates());
+                        } else {
+                            queryString.append("AND e.corporate IN :corporates");
+                            params.put("corporates", role.getCorporates());
+                        }
+                    } else {
+                        queryString.append(" AND e.idCorporate IN :idCorporates");
+                        params.put("idCorporates", role.getCorporates().stream().map(Corporate::getIdCorporate).toList());
+                    }
+                    break;
+                case 'B':
+                    if (!entity.getSimpleName().equals("Branch")) {
+                        queryString.append(" AND e.branch IN :branches");
+                        params.put("branches", role.getBranches());
+                    } else {
+                        queryString.append(" AND e.idBranch IN :idBranches");
+                        params.put("idBranches", role.getBranches().stream().map(Branch::getIdBranch).toList());
+                    }
+                    break;
+                case 'E':
+                    if (!entity.getSimpleName().equals("Branch")) {
+                        queryString.append(" AND e.branch = :branch");
+                        params.put("branch", role.getBranches().getFirst());
+                    } else {
+                        queryString.append(" AND e.idBranch = :idBranch");
+                        params.put("idBranch", role.getBranches().getFirst());
+                    }
+                    break;
+            }
+
+            if (Arrays.stream(entity.getDeclaredFields()).anyMatch(f -> f.getName().equals("active"))) {
+                queryString.append(" AND e.active = true");
+            }
+
+            final TypedQuery<T> query = em.createQuery(queryString.toString(), entity);
+
+            for (Map.Entry<String, Object> entry : params.entrySet()) {
+                query.setParameter(entry.getKey(), entry.getValue());
+            }
+
+            return query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public static <T> T findById(Class<T> entity, Object id) {
+        try (EntityManager em = ConnectionManager.getEntityManager()) {
+            return em.find(entity, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static <T> boolean saveOrUpdate(T entity) {
+        EntityManager em = null;
+
+        EntityTransaction transaction = null;
+
+        try {
+            em = ConnectionManager.getEntityManager();
+            transaction = em.getTransaction();
+            transaction.begin();
+
+            em.merge(entity);
+
+            transaction.commit();
+
+            return true;
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+
+            e.printStackTrace();
+            return false;
+        } finally {
+            ConnectionManager.closeEntityManager(em);
+        }
+    }
+
+    public static <T> boolean delete(Class<T> entity, Integer id) {
+        EntityManager em = ConnectionManager.getEntityManager();
+
+        EntityTransaction transaction = em.getTransaction();
+
+        try {
+            transaction.begin();
+
+            final T entityToDelete = em.find(entity, id);
+
+            em.remove(entityToDelete);
+
+            transaction.commit();
+
+            return true;
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+
+            e.printStackTrace();
+
+            return false;
+        }
+    }
+
+    public static <T> List<T> search(Class<T> entity, String search, String field, Role role) {
+        try {
+            entity.getDeclaredField(field);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        try (EntityManager em = ConnectionManager.getEntityManager()) {
+            final CriteriaBuilder cb = em.getCriteriaBuilder();
+            final CriteriaQuery<T> cq = cb.createQuery(entity);
+            final Root<T> root = cq.from(entity);
+
+            Predicate predicate = cb.like(root.get(field).as(String.class), "%" + search + "%");
+
+            switch (role.getRole()) {
+                case 'A':
+                    break;
+                case 'C':
+                    if (entity.getSimpleName().equals("Corporate")) {
+                        predicate = cb.and(predicate, root.get("idCorporate").in(role.getCorporates().stream().map(Corporate::getIdCorporate).collect(Collectors.toList())));
+                    } else {
+                        boolean hasBranch = false;
+
+                        for (Field f : entity.getDeclaredFields()) {
+                            if (f.getName().equals("branch")) {
+                                hasBranch = true;
+                                break;
+                            }
+                        }
+
+                        if (hasBranch) {
+                            final Join<T, Branch> branchJoin = root.join("branch");
+
+                            predicate = cb.and(predicate, branchJoin.get("corporate").in(role.getCorporates()));
+                        } else {
+                            predicate = cb.and(predicate, root.get("corporate").in(role.getCorporates()));
+                        }
+                    }
+                    break;
+                case 'B':
+                case 'E':
+                    if (entity.getSimpleName().equals("Branch")) {
+                        predicate = cb.and(predicate, root.get("idBranch").in(role.getBranches().stream().map(Branch::getIdBranch).collect(Collectors.toList())));
+                    } else {
+                        predicate = cb.and(predicate, root.get("branch").in(role.getBranches()));
+                    }
+                    break;
+            }
+
+            cq.where(predicate);
+
+            return em.createQuery(cq).getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/main/resources/dumps/dump-uniroom-202402262003.sql
@@ -0,0 +1,291 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: localhost    Database: uniroom
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `branch`
+--
+
+DROP TABLE IF EXISTS `branch`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `branch` (
+  `id_branch` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `id_corporate` int NOT NULL,
+  `id_user` int NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id_branch`),
+  KEY `branch_corporate_FK` (`id_corporate`),
+  KEY `branch_user_FK` (`id_user`),
+  CONSTRAINT `branch_corporate_FK` FOREIGN KEY (`id_corporate`) REFERENCES `corporate` (`id_corporate`),
+  CONSTRAINT `branch_user_FK` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `branch`
+--
+
+LOCK TABLES `branch` WRITE;
+/*!40000 ALTER TABLE `branch` DISABLE KEYS */;
+/*!40000 ALTER TABLE `branch` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `corporate`
+--
+
+DROP TABLE IF EXISTS `corporate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `corporate` (
+  `id_corporate` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `cnpj` char(14) NOT NULL,
+  `id_user` int NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id_corporate`),
+  UNIQUE KEY `corporate_unique` (`cnpj`),
+  KEY `corporate_user_FK` (`id_user`),
+  CONSTRAINT `corporate_user_FK` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `corporate`
+--
+
+LOCK TABLES `corporate` WRITE;
+/*!40000 ALTER TABLE `corporate` DISABLE KEYS */;
+/*!40000 ALTER TABLE `corporate` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventory`
+--
+
+DROP TABLE IF EXISTS `inventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventory` (
+  `id_inventory` int NOT NULL AUTO_INCREMENT,
+  `id_room` int NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id_inventory`),
+  KEY `inventory_room_FK` (`id_room`),
+  CONSTRAINT `inventory_room_FK` FOREIGN KEY (`id_room`) REFERENCES `room` (`id_room`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventory`
+--
+
+LOCK TABLES `inventory` WRITE;
+/*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventory_item`
+--
+
+DROP TABLE IF EXISTS `inventory_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventory_item` (
+  `id_inventory` int NOT NULL,
+  `id_item` int NOT NULL,
+  `quantity` int NOT NULL,
+  UNIQUE KEY `inventory_item_id_inventory_IDX` (`id_inventory`,`id_item`) USING BTREE,
+  KEY `inventory_item_item_FK` (`id_item`),
+  CONSTRAINT `inventory_item_inventory_FK` FOREIGN KEY (`id_inventory`) REFERENCES `inventory` (`id_inventory`),
+  CONSTRAINT `inventory_item_item_FK` FOREIGN KEY (`id_item`) REFERENCES `item` (`id_item`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventory_item`
+--
+
+LOCK TABLES `inventory_item` WRITE;
+/*!40000 ALTER TABLE `inventory_item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inventory_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `id_item` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `price` float NOT NULL,
+  `id_branch` int NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id_item`),
+  KEY `item_branch_FK` (`id_branch`),
+  CONSTRAINT `item_branch_FK` FOREIGN KEY (`id_branch`) REFERENCES `branch` (`id_branch`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservation`
+--
+
+DROP TABLE IF EXISTS `reservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservation` (
+  `id_reservation` int NOT NULL AUTO_INCREMENT,
+  `id_room` int NOT NULL,
+  `days` tinyint NOT NULL DEFAULT '1',
+  `id_user` int NOT NULL,
+  `status` varchar(2) NOT NULL DEFAULT 'CI',
+  `id_branch` int NOT NULL,
+  PRIMARY KEY (`id_reservation`),
+  KEY `reservation_room_FK` (`id_room`),
+  KEY `reservation_user_FK` (`id_user`),
+  KEY `reservation_branch_FK` (`id_branch`),
+  CONSTRAINT `reservation_branch_FK` FOREIGN KEY (`id_branch`) REFERENCES `branch` (`id_branch`),
+  CONSTRAINT `reservation_room_FK` FOREIGN KEY (`id_room`) REFERENCES `room` (`id_room`),
+  CONSTRAINT `reservation_user_FK` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservation`
+--
+
+LOCK TABLES `reservation` WRITE;
+/*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `room` (
+  `id_room` int NOT NULL AUTO_INCREMENT,
+  `room_number` int NOT NULL,
+  `id_room_type` int NOT NULL,
+  `id_branch` int NOT NULL,
+  PRIMARY KEY (`id_room`),
+  KEY `room_room_type_FK` (`id_room_type`),
+  KEY `room_branch_FK` (`id_branch`),
+  CONSTRAINT `room_branch_FK` FOREIGN KEY (`id_branch`) REFERENCES `branch` (`id_branch`),
+  CONSTRAINT `room_room_type_FK` FOREIGN KEY (`id_room_type`) REFERENCES `room_type` (`id_room_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room_type`
+--
+
+DROP TABLE IF EXISTS `room_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `room_type` (
+  `id_room_type` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `capacity` tinyint NOT NULL,
+  `price` float NOT NULL,
+  `id_branch` int NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id_room_type`),
+  KEY `roomt_type_branch_FK` (`id_branch`),
+  CONSTRAINT `roomt_type_branch_FK` FOREIGN KEY (`id_branch`) REFERENCES `branch` (`id_branch`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room_type`
+--
+
+LOCK TABLES `room_type` WRITE;
+/*!40000 ALTER TABLE `room_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `room_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` text NOT NULL,
+  `role` char(1) NOT NULL DEFAULT 'E',
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `user_unique` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'uniroom'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-26 20:03:58
